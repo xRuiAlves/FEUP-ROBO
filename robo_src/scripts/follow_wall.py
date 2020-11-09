76fdcae3ce7c0d27b00dad91cffae202daf73ca9
Rui Alves <up201606746@fe.up.pt>
2020-11-09 13:19:47 +0000

    Add first iteration of wall following

--- robo_src/scripts/follow_wall.py
+++ robo_src/scripts/follow_wall.py
@@ -6,15 +6,15 @@ from geometry_msgs.msg import Twist
 
 # constants
 TURNING_FACTOR = 1
-ROBOT_SPEED = 0.5
+ROBOT_SPEED = 0.25
 TARGET_WALL_DISTANCE = 1
 TARGET_DIAGONAL_DISTANCE = math.sqrt(math.pow(TARGET_WALL_DISTANCE, 2) + math.pow(TARGET_WALL_DISTANCE, 2))
 
 # sensors
 SENSORS = {
     "SENSOR_FL": 45,     # front-left
-    "SENSOR_BL": 135,    # back-left
-    "SENSOR_BR": 225,    # back-right
+    # "SENSOR_BL": 135,    # back-left
+    # "SENSOR_BR": 225,    # back-right
     "SENSOR_FR": 315     # front-right
 }
 
@@ -36,24 +36,23 @@ def getSensorDistances(sensor_data):
 def isMeasureValid(sensor_data, measure):
     return measure >= sensor_data.range_min and measure <= sensor_data.range_max
 
-def callback(sensor_data):
-    while True:
-        [fl, bl, br, fr] = getSensorDistances(sensor_data)
-        # rospy.loginfo([fl, bl, br, fr])
+def callback(sensor_data, pub):
+    [fl, fr] = getSensorDistances(sensor_data)
+    spin = 0
 
-        if (isMeasureValid(sensor_data, fl)):
-            rospy.loginfo(f"fl is valid: {fl}")
-        if (isMeasureValid(sensor_data, bl)):
-            rospy.loginfo(f"bl is valid: {bl}")
-        if (isMeasureValid(sensor_data, br)):
-            rospy.loginfo(f"br is valid: {br}")
-        if (isMeasureValid(sensor_data, fr)):
-            rospy.loginfo(f"fr is valid: {fr}")
+    if (isMeasureValid(sensor_data, fl)):
+        spin += fl - TARGET_DIAGONAL_DISTANCE
+    if (isMeasureValid(sensor_data, fr)):
+        spin += TARGET_DIAGONAL_DISTANCE - fr
+
+    rospy.loginfo([fl, fr])
+    moveRobot(pub, spin)
+
     
 def listener():
     rospy.init_node('robot', anonymous=True)
 
-    sub = rospy.Subscriber('/scan', LaserScan, callback)
+    sub = rospy.Subscriber('/scan', LaserScan, lambda sensor_data: callback(sensor_data, pub))
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     rospy.spin()
