b7282a51a3dcbcee0ddd5a3830cf760057764359
Rui Alves <up201606746@fe.up.pt>
2020-11-09 17:15:10 +0000

    Add robot turning capacity on edges (left and right sensors)

--- robo_src/scripts/follow_wall.py
+++ robo_src/scripts/follow_wall.py
@@ -5,49 +5,67 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 # constants
-TURNING_FACTOR = 1
-ROBOT_SPEED = 0.25
 TARGET_WALL_DISTANCE = 1
-TARGET_DIAGONAL_DISTANCE = math.sqrt(math.pow(TARGET_WALL_DISTANCE, 2) + math.pow(TARGET_WALL_DISTANCE, 2))
+ROBOT_SPEED = TARGET_WALL_DISTANCE / 2
+
+TARGET_DIAGONAL_DISTANCE = TARGET_WALL_DISTANCE / math.sin(math.pi / 4)
+
+TURNING_FACTOR = 0.65
+TURNING_SPEED = (ROBOT_SPEED * TURNING_FACTOR) / TARGET_WALL_DISTANCE
 
 # sensors
 SENSORS = {
     "SENSOR_FL": 45,     # front-left
-    # "SENSOR_BL": 135,    # back-left
-    # "SENSOR_BR": 225,    # back-right
+    "SENSOR_L": 90,      # left
+    "SENSOR_R": 270,     # right
     "SENSOR_FR": 315     # front-right
 }
 
-def moveRobot(pub, spin):
+def moveRobot(pub, rotation):
     msg = Twist()
     msg.linear.x = ROBOT_SPEED
-    msg.angular.z = spin
+    msg.angular.z = rotation
     pub.publish(msg)
 
 def getSensorDistance(sensor_data, angle):
-    return sensor_data.ranges[angle]
-
+    distance = sensor_data.ranges[angle]
+    return distance if isDistanceValid(sensor_data, distance) else None
+        
 def getSensorDistances(sensor_data):
     return list(map(
         lambda angle: getSensorDistance(sensor_data, angle),
         SENSORS.values()
     ))
 
-def isMeasureValid(sensor_data, measure):
-    return measure >= sensor_data.range_min and measure <= sensor_data.range_max
+def isDistanceValid(sensor_data, distance):
+    return distance >= sensor_data.range_min and distance <= sensor_data.range_max
 
 def callback(sensor_data, pub):
-    [fl, fr] = getSensorDistances(sensor_data)
-    spin = 0
+    [fl, l, r, fr] = getSensorDistances(sensor_data)
 
-    if (isMeasureValid(sensor_data, fl)):
-        spin += fl - TARGET_DIAGONAL_DISTANCE
-    if (isMeasureValid(sensor_data, fr)):
-        spin += TARGET_DIAGONAL_DISTANCE - fr
+    should_follow_wall = (fl != None) or (fr != None)
+    should_turn = (not should_follow_wall) and ((l != None) or (r != None))
 
-    rospy.loginfo([fl, fr])
-    moveRobot(pub, spin)
+    rotation = 0
 
+    if (should_follow_wall):
+        rospy.loginfo("Following wall")
+        if (fl != None):
+            rospy.loginfo(f"\tfront_left = {fl}")
+            rotation += fl - TARGET_DIAGONAL_DISTANCE
+        if (fr != None):
+            rospy.loginfo(f"\tfront_right = {fr}")
+            rotation -= fr - TARGET_DIAGONAL_DISTANCE
+    elif (should_turn):
+        rospy.loginfo("Turning")
+        if (l != None):
+            rotation = TURNING_SPEED
+            rospy.loginfo("Turning left")
+        elif (r != None):
+            rotation = -TURNING_SPEED
+            rospy.loginfo("Turning right")
+
+    moveRobot(pub, rotation)
     
 def listener():
     rospy.init_node('robot', anonymous=True)
