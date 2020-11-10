fc5d2c30cdccd68e29ef01392012899cb8724f2a
Rui Alves <up201606746@fe.up.pt>
2020-11-10 20:21:42 +0000

    Improve logics to handle convex and concave curves

--- robo_src/scripts/follow_wall.py
+++ robo_src/scripts/follow_wall.py
@@ -6,20 +6,38 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from statistics import mean
 
-# constants
-TARGET_WALL_DISTANCE = 0.16
-ROBOT_SPEED = 0.05
-P_WEIGHT = 3
-D_WEIGHT = 1
-# D_MIN = -1
-# D_MAX = 1
-NUM_REGRESSION_POINTS = 15
+# robot dicision sensors
+SENSORS = {
+    "FRONT": 0,
+    "FRONTAL_LEFT": 45,
+    "FRONTAL_RIGHT": 315,
+    "LEFT": 90,
+    "RIGHT": 270,
+    "BACK_LEFT": 105,
+    "BACK_RIGHT": 255
+}
 
-def moveRobot(pub, rotation):
+# constants
+TARGET_WALL_DISTANCE = 0.17
+ROBOT_SPEED = 0.15
+
+DISTANCE_THRESHOLD = 2 * TARGET_WALL_DISTANCE
+DISTACE_DIFF_WEIGHT = 3
+LIN_REGRESSION_WEIGHT = 5
+NUM_REGRESSION_POINTS = 25
+
+TURNING_FACTOR = ROBOT_SPEED * TARGET_WALL_DISTANCE * 5/4 * math.pi
+TURNING_SPEED = TURNING_FACTOR * 2 * math.pi
+
+def moveRobot(pub, speed, rotation):
     msg = Twist()
-    msg.linear.x = ROBOT_SPEED
+    msg.linear.x = speed
     msg.angular.z = rotation
     pub.publish(msg)
+
+def getAngleDistance(sensor_data, angle):
+    distance = sensor_data.ranges[angle]
+    return distance if isDistanceValid(sensor_data, distance) else None
 
 def getClosestAngle(sensor_data):
     closest_angle = None
@@ -33,7 +51,7 @@ def getClosestAngle(sensor_data):
 
     return closest_angle
 
-def getClosestPoints(sensor_data):
+def calcClosestPoints(sensor_data):
     closest_angle = getClosestAngle(sensor_data)
     if (closest_angle == None):
         return []
@@ -41,9 +59,9 @@ def getClosestPoints(sensor_data):
     distances = ((angle, sensor_data.ranges[angle]) for angle in range(len(sensor_data.ranges)))
     valid_distances = list(filter(lambda ang_dist: isDistanceValid(sensor_data, ang_dist[1]), distances))
     closest_distances = sorted(valid_distances, key=lambda ang_dist: ang_dist[1])[:NUM_REGRESSION_POINTS]
-    return list(map(lambda ang_dist: getPoint(ang_dist[0], ang_dist[1]), closest_distances))
+    return list(map(lambda ang_dist: calcPoint(ang_dist[0], ang_dist[1]), closest_distances))
 
-def getPoint(angle, distance):
+def calcPoint(angle, distance):
     x = distance * math.cos(degreeToRad(angle))
     y = distance * math.sin(degreeToRad(angle))
     return (x, y)
@@ -52,9 +70,9 @@ def degreeToRad(ang_degrees):
     return (ang_degrees / 180) * math.pi
 
 def isDistanceValid(sensor_data, distance):
-    return distance != None and distance >= sensor_data.range_min and distance < sensor_data.range_max
+    return distance != None and distance > sensor_data.range_min and distance < sensor_data.range_max
 
-def findLinearRegressionSlope(points):
+def calcLinearRegressionSlope(points):
     points_x = list(map(lambda point: point[0], points))
     points_y = list(map(lambda point: point[1], points))
 
@@ -63,37 +81,68 @@ def findLinearRegressionSlope(points):
 
     return (((mean(xs)*mean(ys)) - mean(xs*ys)) / ((mean(xs)**2) - mean(xs**2)))
 
-def calcP(sensor_data):
+def calcDistanceDiff(sensor_data):
     closest_angle = getClosestAngle(sensor_data)
     if (closest_angle == None):
         return 0
 
-    rospy.loginfo(f"ang : {closest_angle}")
     distance = sensor_data.ranges[closest_angle]
-    rospy.loginfo(f"dist: {distance}")
     diff = distance - TARGET_WALL_DISTANCE
 
     turning_sign = 1 if (closest_angle >= 0 and closest_angle <= 180) else -1
-    return diff * turning_sign
+    return diff * TURNING_SPEED * turning_sign * 2 * math.pi
 
-def calcDist(point):
-    return math.sqrt(point[0]**2 + point[1]**2)
-
-def calcD(sensor_data):
-    points = getClosestPoints(sensor_data)
-    slope = findLinearRegressionSlope(points)
+def calcLinRegresionSlope(sensor_data):
+    points = calcClosestPoints(sensor_data)
+    slope = calcLinearRegressionSlope(points)
     return slope
 
+def getAngIntervalDistSum(sensor_data, ang_from, ang_to):
+    distances = sensor_data.ranges[ang_from : ang_to]
+    return sum(filter(lambda distance: isDistanceValid(sensor_data, distance), distances))
+
+def calcSpeedRotation(sensor_data):
+    f = getAngleDistance(sensor_data, SENSORS["FRONT"])
+    fl = getAngleDistance(sensor_data, SENSORS["FRONTAL_LEFT"])
+    fr = getAngleDistance(sensor_data, SENSORS["FRONTAL_RIGHT"])
+    l = getAngleDistance(sensor_data, SENSORS["LEFT"])
+    r = getAngleDistance(sensor_data, SENSORS["RIGHT"])
+
+    has_frontal_diagonals = fl != None or fr != None
+    frontal_collision_danger = f != None and f <= DISTANCE_THRESHOLD
+
+    has_close_left_wall = (l != None and l < DISTANCE_THRESHOLD)
+    has_close_right_wall = (r != None and r < DISTANCE_THRESHOLD)
+    should_turn_left = has_close_left_wall and (fl == None or fl >= DISTANCE_THRESHOLD)
+    should_turn_right = has_close_right_wall and (fr == None or fr >= DISTANCE_THRESHOLD)
+
+    if (frontal_collision_danger):
+        rospy.loginfo(f"FRONTAL_COLLISION")
+        bl = getAngleDistance(sensor_data, SENSORS["BACK_LEFT"])
+        br = getAngleDistance(sensor_data, SENSORS["BACK_RIGHT"])
+        should_turn_left = bl == None or (br != None and bl > br)
+        return 0, 1.5 * TURNING_SPEED * (1 if should_turn_left else -1)
+    if (should_turn_left):
+        rospy.loginfo(f"TURNING LEFT")
+        return ROBOT_SPEED, TURNING_SPEED
+    if (should_turn_right):
+        rospy.loginfo(f"TURNING RIGHT")
+        return ROBOT_SPEED, -TURNING_SPEED
+    if (has_close_left_wall or has_close_right_wall):
+        rospy.loginfo(f"FOLLOWING WALL")
+        distance_diff = calcDistanceDiff(sensor_data) * DISTACE_DIFF_WEIGHT
+        lin_regression_slope = calcLinRegresionSlope(sensor_data) * LIN_REGRESSION_WEIGHT
+        return 1.5 * ROBOT_SPEED, distance_diff + lin_regression_slope
+    else:
+        rospy.loginfo(f"FAK\n\tl = {l}\n\tr = {r}\n\tfl = {fl}\n\tfr = {fr}")
+        return ROBOT_SPEED, 0
+
 def callback(sensor_data, pub):
-    P = calcP(sensor_data) * P_WEIGHT
-    D = calcD(sensor_data) * D_WEIGHT
-
-    rospy.loginfo(f"P: {P}")
-    rospy.loginfo(f"D: {D}")
-    rospy.loginfo(f"---------------------")
-
-    rotation = P + D
-    moveRobot(pub, rotation)
+    speed, rotation = calcSpeedRotation(sensor_data)
+    rospy.loginfo(f"Speed = {speed}")
+    rospy.loginfo(f"Rotation = {rotation}")
+    rospy.loginfo("--------------------")
+    moveRobot(pub, speed, rotation)
     
 def listener():
     rospy.init_node('robot', anonymous=True)
