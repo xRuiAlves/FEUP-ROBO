1ef80ee182d42a76482b133cf2b599057ed05275
miguelpduarte <miguelpduarte98@gmail.com>
2020-12-13 20:12:47 +0000

    Update rocker run scripts with correct volume paths

--- docker_images/ros1/rocker_run.sh
+++ docker_images/ros1/rocker_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-rocker --oyr-run-arg " -v $(pwd)/../robo_src/:/root/catkin_ws/src/robo/" --devices /dev/dri/card0 --x11 robo:turtled 
+rocker --oyr-run-arg " -v $(pwd)/../../robo_src/:/root/catkin_ws/src/robo/" --devices /dev/dri/card0 --x11 robo:turtled 
 # rocker --devices /dev/dri/card0 --x11 robo:turtled 
--- docker_images/ros2/rocker_run.sh
+++ docker_images/ros2/rocker_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # rocker --oyr-run-arg " -v $(pwd)/../robo_src/:/root/catkin_ws/src/robo/" --devices /dev/dri/card0 --x11 robo2:turtled 
 # rocker --devices /dev/dri/card0 --x11 robo2:turtled 
-rocker --devices /dev/dri/card0 --x11 robo2:full
+rocker --oyr-run-arg " --name robo-rocker -v $(pwd)/../../robo2_src/:/root/robo/src/robo_fi/" --devices /dev/dri/card0 --x11 robo2:full
